58f5d7c7d81e34bcfa8747f8f2b9620f805542e2
Arun-CJ <arunc38@gmail.com>
2019-11-20 16:46:13 +0530

    "delete functionality added"

--- src/components/history/tasks.js
+++ src/components/history/tasks.js
@@ -222,7 +222,31 @@ class Tasks extends Component {
     
   }
 
-  
+  handleDelete = (e, row, idx) =>{
+    console.log("deleting",e.target)
+    const {projId, statusId, addRows} = this.state
+    const taskStatus = 
+      {
+        status_id:idx.status_id,
+        task_id:idx.task_id
+      }
+  console.log(idx)
+    axios
+    .post(`${API_URL}/api/deleteTask` , taskStatus ,{
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token")
+      }
+    })
+    .then(res =>{
+      console.log(res);
+      this.setState(prevState =>({
+        addRows: prevState.addRows.filter(m => m.task_id !== taskStatus.task_id),
+        editedERow:false
+      }))
+    })
+    .catch(err => console.log(err))
+    }
+
 
   handleCancel = (e) =>{
     this.setState({
@@ -365,7 +389,7 @@ class Tasks extends Component {
                             :
                             <div>
                             <button className="btn btn-success" onClick={(e)=>this.handleEdit(e,index,item)}><i className="fa fa-edit"></i></button>
-                            {/* <button className="btn btn-danger" onClick={(e)=>this.handleDelete(e,index,item)}><i className="fa fa-trash"></i></button> */}
+                            <button className="btn btn-danger" onClick={(e)=>this.handleDelete(e,index,item)}><i className="fa fa-trash"></i></button>
                             </div>
                           }
                           
