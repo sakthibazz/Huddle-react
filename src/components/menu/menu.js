a698e424be248737de7cce646341cf503505ed2b
Ramesh <ramesh@aroha.co.in>
2019-12-05 14:47:33 +0530

    Docker files

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,51 +1,49 @@
 import React,{ Component } from 'react'
 import {  Link , NavLink ,withRouter} from 'react-router-dom';
-import './menu.css';
+// import './menu.css';
 
 
 class Menu extends Component{
 
-  handleLogout = (e) =>{
-    localStorage.clear();
-    this.props.history.push('/landing')
-    
-  }
-    render(){
-        return (
-            <div>
-                <nav class="navbar navbar-expand-lg fixed-top">
-    <div class="container">
-        <Link class="navbar-brand" to="/">Aroha Technologies</Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="nav pl-5">
-            
-             <li className="dropdown pt-2">
-                 <Link to='/dashboard' className='dropdown-toggle' data-toggle="dropdown" data-toggle="dropdown-hover">User Management</Link>
-                                    <ul className="dropdown-menu ">
-                                        <li><Link to='/admin' className="dropdown-item" >Admin</Link></li>
-                                        <li><Link to='/managers' className="dropdown-item" >Managers</Link></li>
-                                        <li><Link to='/departments' className="dropdown-item" >Departments/Projects</Link></li>
-                                    </ul>
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg fixed-top navbar-custom sticky sticky-dark">
+        <div className="container">
+          <a className="navbar-brand logo text-uppercase" href="">
+            Huddle Meeting
+                </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i className="mdi mdi-menu"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav navbar-center" id="mySidenav">
+              <li className="nav-item active">
+                <a href="#home" className="nav-link">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="#workflow" className="nav-link">Workflow</a>
+              </li>
 
+              <li className="nav-item">
+                <a href="#team" className="nav-link">Team</a>
+              </li>
 
-                                </li>
-                                <li class="nav-item">
-              <Link class="nav-link" to="/meetings">Meetings </Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" onClick={(e) => this.handleLogout(e)}>Logout </Link>
-            </li>
-            
-          </ul>
+              <li className="nav-item">
+                <a href="#contact" className="nav-link">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a href="/register" className="nav-link">Register</a>
+
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link">Login</a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
-  </nav>
-            </div>
-        )
-    }
+      </nav>
+    );
+  }
 }
 
 export default withRouter(Menu)
